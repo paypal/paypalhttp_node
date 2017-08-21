63b176383262dd67acd9debeda20f7be373ca4b9
sashaweiss <sasha@sashaweiss.coffee>
2017-08-21 16:20:10 -0700

    Releasinator setup

---
+++ .releasinator.rb
@@ -0,0 +1,45 @@
+configatron.product_name = "BraintreeHttp Node"
+
+# Custom validations
+def test
+  tag = Time.now.to_i
+  _test_with_dockerfile(tag)
+end
+
+def _test_with_dockerfile(tag)
+  CommandProcessor.command("docker build -t #{tag} .")
+  CommandProcessor.command("docker run #{tag}", live_output=true)
+end
+
+configatron.custom_validation_methods = [
+	method(:validate_version_match),
+  method(:test)
+]
+
+# Build, update version, and publish to NPM
+def build_method
+	# noop
+end
+
+configatron.build_method = method(:build_method)
+
+def update_version_method(version, semver_type)
+	CommandProcessor.command("npm version --no-git-tag-version #{semver_type}")
+end
+
+configatron.update_version_method = method(:update_version_method)
+
+def publish_to_package_manager(version)
+	CommandProcessor.command("npm publish")
+end
+
+configatron.publish_to_package_manager_method = method(:publish_to_package_manager)
+
+def wait_for_package_manager(version)
+end
+
+configatron.wait_for_package_manager_method = method(:wait_for_package_manager)
+
+# Miscellania
+configatron.prerelease_checklist_items = []
+configatron.release_to_github = true
---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'rake'
+
+spec = Gem::Specification.find_by_name 'releasinator'
+load "#{spec.gem_dir}/lib/tasks/releasinator.rake"
+
+task :test do
+  sh "python -m unittest tests"
+end
