d85be483f9525d71019e1f85f18897fe8994053a
bluk and jackellenberger <bryant.luk@getbraintree.com>
2018-03-26 21:51:17 +0000

    Add support for multipart/related
    - Use `multipart/related` but send as `multipart/form-data`
    - For JSON data, send as a fake file that has a Content-Type
    `application/json`
    - Order the parts so real files are added last
    - Add support for image/gif

--- spec/unit/http_client_spec.js
+++ spec/unit/http_client_spec.js
@@ -226,6 +226,82 @@ describe('HttpClient', function () {
       });
     });
 
+    it('serializes multipart/related request correctly', function () {
+      let request = {
+        verb: 'POST',
+        path: '/',
+        headers: {
+          'Content-Type': 'multipart/related'
+        },
+        partHeaders: {
+          input_field_name: {
+            'Content-Type': 'application/json'
+          }
+        },
+        body: {
+          file1: fs.createReadStream('./spec/unit/resources/fileupload_test_binary.jpg'),
+          file2: fs.createReadStream('./spec/unit/resources/evidence.pdf'),
+          input_field_name: {
+            foo: "bar"
+          }
+        }
+      };
+
+      this.context.post('/').reply(200, function (uri, body) {
+        let bb = new Busboy({headers: this.req.headers});
+        assert.match(this.req.headers['content-type'], /^multipart\/form-data; boundary=/);
+
+        let imageFileChecked = false;
+        let pdfFileChecked = false;
+        let jsonChecked = false;
+        bb.on('file', (fieldname, file, filename, encoding, mimetype) => {
+          if (filename === 'fileupload_test_binary.jpg') {
+            file.on('data', (data) => {
+              assert.isTrue(jsonChecked); // ensure JSON is sent first
+
+              assert.equal(mimetype, "image/jpeg");
+              assert.equal(fieldname, 'file1');
+              assert.equal(data.length, 1132);
+
+              imageFileChecked = true;
+            });
+          } else if (filename === 'input_field_name.json') {
+            file.on('data', (data) => {
+              assert.equal(mimetype, "application/json");
+              assert.equal(fieldname, 'input_field_name');
+              assert.equal(data, '{"foo":"bar"}');
+
+              jsonChecked = true;
+            });
+          } else if (filename === 'evidence.pdf') {
+            file.on('data', (data) => {
+              assert.isTrue(jsonChecked); // ensure JSON is sent first
+
+              assert.equal(mimetype, "application/pdf");
+              assert.equal(fieldname, 'file2');
+              assert.equal(data.length, 31376);
+
+              pdfFileChecked = true;
+            });
+          }
+        });
+
+        bb.on('finish', () => {
+          assert.isTrue(imageFileChecked);
+          assert.isTrue(jsonChecked);
+          assert.isTrue(pdfFileChecked);
+        });
+
+        let stream = Buffer.from(body, 'hex');
+
+        bb.end(stream);
+      });
+
+      return this.http.execute(request).then((resp) => {
+        assert.equal(resp.statusCode, 200);
+      });
+    });
+
     it('parses 200-level response', function () {
       let http = new braintreehttp.HttpClient(environment);
       let request = {
